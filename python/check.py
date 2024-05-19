ccb7f303f7791daaa31d6a352e10a3549b2bfab8
kulbeers <kulbeers@vehant.com>
2024-05-19 20:05:34 +0530

    First_Commit

---
+++ python/check.py
@@ -0,0 +1,5 @@
+import os
+filePath = '/var/kritiscan/conf/xconfig.xml_dual_Endurance_Test'
+readFile = open(filePath,'r')
+print(readFile)
+readFile.close()
---
+++ python/string.py
@@ -0,0 +1,6 @@
+def sum(a,b):
+ c=a+b
+ return c
+
+d=sum(2,3)
+print(d)
